6881802771fe6c9ed89071659dec92ddd5ba3966
569411017 <569411017@qq.com>
2019-08-13 15:29:12 +0800

    ribbon-restTemplate-hystrixComand

--- eureka-demo/eureka-demo-parent/eureka-ribbon-client/src/main/java/com/sitaluo/EurekaRibbonClientApplication.java
+++ eureka-demo/eureka-demo-parent/eureka-ribbon-client/src/main/java/com/sitaluo/EurekaRibbonClientApplication.java
@@ -3,10 +3,12 @@ package com.sitaluo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class EurekaRibbonClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaRibbonClientApplication.class, args);
--- eureka-demo/eureka-demo-parent/eureka-ribbon-client/src/main/java/com/sitaluo/service/HiRibbonService.java
+++ eureka-demo/eureka-demo-parent/eureka-ribbon-client/src/main/java/com/sitaluo/service/HiRibbonService.java
@@ -4,13 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @Component
 public class HiRibbonService {
 
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@HystrixCommand(fallbackMethod="hiError")
 	public String hi(String name) {
 		return restTemplate.getForObject("http://eureka-client/hi?name="+name, String.class);
 	}
+	
+	public String hiError(String name) {
+		return "hi,"+name+", error!";
+	}
+	
 }
